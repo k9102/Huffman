c43d581446868600d76aeb1a748dd8c71ea1bd0c
김규열(k9102) <k9102@lginnotek.com>
2019-02-21 18:32:26 +0900

    memory check is added

--- Huffman.cpp
+++ Huffman.cpp
@@ -2,6 +2,10 @@
 //
 
 #include "pch.h"
+#define _CRTDBG_MAP_ALLOC  
+#include <stdlib.h>  
+#include <crtdbg.h> 
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -12,46 +16,37 @@
 
 using namespace std;
 
+class Node;
+using NodePtr = shared_ptr<Node>;
 
 class Node
 {
 public:
 	char val_;
 	int freq_;
-	shared_ptr<Node> l_ = nullptr;
-	shared_ptr<Node> r_ = nullptr;
-	Node(char val, int freq, shared_ptr<Node> l=nullptr, shared_ptr<Node> r=nullptr) : val_(val),freq_(freq),l_(l),r_(r) {}
+	NodePtr l_;
+	NodePtr r_;
+	Node(char val, int freq, NodePtr l = nullptr, NodePtr r = nullptr) : val_(val), freq_(freq), l_(l), r_(r) {}
 };
 
-string str_ = R"(
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-)";
-
-string str = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff";
-map<char, string> code;
-void Traverse(shared_ptr<Node> tree,string prefix)
+string str = R"(aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff)";
+map<char, string> code_map;
+void Traverse(NodePtr tree, string prefix)
 {
-	if (tree == nullptr)
+	if (nullptr == tree)
 	{
 		return;
 	}
-	if (tree->val_ != 0)
+	if (0 != tree->val_)
 	{
-		code[tree->val_] = prefix;
-		return;
+		code_map[tree->val_] = prefix;
 	}
-
-	Traverse(tree->l_, prefix + "0");
-	Traverse(tree->r_, prefix + "1");
+	else
+	{
+		Traverse(tree->l_, prefix + "0");
+		Traverse(tree->r_, prefix + "1");
+	}
 }
 
 /*
@@ -72,7 +67,7 @@ Input is array of unique characters along with their frequency of occurrences an
 4. Repeat steps#2 and #3 until the heap contains only one node.The remaining node is the root node and the tree is complete.
 */
 
-int main()
+void GenerateHuffCode()
 {
 	map<char, int> tbl;
 
@@ -81,9 +76,9 @@ int main()
 		tbl[c]++;
 	}
 
-	auto cmp = [](shared_ptr<Node> fir, shared_ptr<Node> sec) { return  fir->freq_ > sec->freq_; };
-	std::priority_queue<shared_ptr<Node>, vector<shared_ptr<Node>>, decltype(cmp)> que(cmp);
-	
+	auto cmp = [](NodePtr fir, NodePtr sec) { return  fir->freq_ > sec->freq_; };
+	std::priority_queue<NodePtr, vector<NodePtr>, decltype(cmp)> que(cmp);
+
 	/*
 	1) Build a Huffman Tree from input characters
 	*/
@@ -100,7 +95,7 @@ int main()
 		auto l = que.top(); que.pop();
 		auto r = que.top(); que.pop();
 
-		int t_freq = l->freq_ + r->freq_; 
+		int t_freq = l->freq_ + r->freq_;
 		//3. Create a new internal node with frequency equal to the sum of the two nodes frequencies.Make the first extracted node as its left child and the other extracted node as its right child.Add this node to the min heap.
 		que.push(make_shared<Node>(0, t_freq, l, r));
 
@@ -109,11 +104,34 @@ int main()
 	/*
 	2) Traverse the Huffman Tree and assign codes to characters
 	*/
-	Traverse(que.top(),"");
+	Traverse(que.top(), "");
 
-	for (auto[val, word] : code)
+	for (auto[val, bits] : code_map)
 	{
-		cout << val << ":"+word << endl;
+		cout << val << ":" + bits << endl;
 	}
+
+	code_map.clear();
+}
+
+int main()
+{
+
+#if 0
+	_CrtSetBreakAlloc(188);
+#endif
+
+	_CrtMemState s1, s2,s3;
+
+	_CrtMemCheckpoint(&s1);
+	GenerateHuffCode();
+	_CrtMemCheckpoint(&s2);
+
+	if (_CrtMemDifference(&s3, &s1, &s2))
+	{
+		_CrtMemDumpStatistics(&s3);
+		_CrtDumpMemoryLeaks();
+	}
+
 }
 
